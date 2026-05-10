9543437cf791703ce02771064aad279fc2010237
Bartosz Trusiński <bartosztrusinski@gmail.com>
2026-05-10 14:29:39 +0200

    add article and profile api data types

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export type Article = {
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  tagList: string[];
+  createdAt: string;
+  updatedAt: string;
+  favorited: boolean;
+  favoritesCount: number;
+  author: Profile;
+};
+
+export type Profile = {
+  username: string;
+  bio: string;
+  image: string;
+  following: boolean;
+};
