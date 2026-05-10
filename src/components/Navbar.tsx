c65791bdc6e6c50310b20bb65ff1b1bc66a10f91
Bartosz Trusiński <bartosztrusinski@gmail.com>
2026-05-10 19:54:37 +0200

    conditionally render navbar links based on auth state

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,9 @@
+import { useAuth } from "contexts/AuthContext";
+import userImagePlaceholder from "assets/user-image-placeholder.png";
+
 export function Navbar() {
+  const { currentUser } = useAuth();
+
   return (
     <nav className="navbar navbar-light">
       <div className="container">
@@ -12,28 +17,47 @@ export function Navbar() {
               Home
             </a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/#/editor">
-              <i className="ion-compose" />
-              &nbsp;New Article
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/#/settings">
-              <i className="ion-gear-a" />
-              &nbsp;Settings
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/#/login">
-              Sign in
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/#/register">
-              Sign up
-            </a>
-          </li>
+
+          {currentUser ? (
+            <>
+              <li className="nav-item">
+                <a className="nav-link" href="/#/editor">
+                  <i className="ion-compose" />
+                  &nbsp;New Article
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#/settings">
+                  <i className="ion-gear-a" />
+                  &nbsp;Settings
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href={`/#/profile/${currentUser.username}`}>
+                  <img src={currentUser.image || userImagePlaceholder} className="user-pic" alt="" />
+                  {currentUser.username}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#/logout">
+                  Logout
+                </a>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="nav-item">
+                <a className="nav-link" href="/#/login">
+                  Sign in
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#/register">
+                  Sign up
+                </a>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
