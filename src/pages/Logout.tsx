38ef94ddffa1a913415792d7b821affef55593af
Bartosz Trusiński <bartosztrusinski@gmail.com>
2026-05-10 20:43:08 +0200

    implement logout functionality and redirect to home page

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,3 +1,16 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+import { useAuth } from "contexts/AuthContext";
+
 export function Logout() {
-  return <></>;
+  const { logOut } = useAuth();
+  const history = useHistory();
+
+  useEffect(() => {
+    logOut();
+    history.push("/");
+  }, [logOut, history]);
+
+  return null;
 }
