52b106b1702798797c64be7a71e467a8a2f9f0ce
Bartosz Trusiński <bartosztrusinski@gmail.com>
2026-05-10 17:20:21 +0200

    fetch and display article; handle loading and errors; use react-markdown to render article body as JSX

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,28 +1,67 @@
-export function Article() {
+import { useQuery } from "@tanstack/react-query";
+import { Link, RouteComponentProps } from "react-router-dom";
+import { format } from "date-fns";
+import Markdown from "react-markdown";
+
+import type { Article as ArticleType } from "types";
+import userImagePlaceholder from "assets/user-image-placeholder.png";
+
+export function Article({ match }: RouteComponentProps<{ slug: string }>) {
+  const { slug } = match.params;
+  const { data, isLoading, isError } = useQuery<{ article: ArticleType }>({
+    queryKey: ["article"],
+    queryFn: async () => {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/articles/${slug}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch article");
+      }
+      return response.json();
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <div className="article-page">
+        <div className="container page">Loading article...</div>
+      </div>
+    );
+  }
+
+  if (isError || !data) {
+    return (
+      <div className="article-page">
+        <div className="container page">Could not load article. Please try again.</div>
+      </div>
+    );
+  }
+
+  const { article } = data;
+
   return (
     <div className="article-page">
       <div className="banner">
         <div className="container">
-          <h1>How to build webapps that scale</h1>
+          <h1>{article.title}</h1>
 
           <div className="article-meta">
-            <a href="/#/profile/ericsimmons">
-              <img src="http://i.imgur.com/Qr71crq.jpg" />
-            </a>
+            <Link to={`/profile/${article.author.username}`}>
+              <img src={article.author.image || userImagePlaceholder} alt="" />
+            </Link>
             <div className="info">
-              <a href="/#/profile/ericsimmons" className="author">
-                Eric Simons
-              </a>
-              <span className="date">January 20th</span>
+              <Link to={`/profile/${article.author.username}`} className="author">
+                {article.author.username}
+              </Link>
+              <span className="date">{format(new Date(article.createdAt), "MMMM do")}</span>
             </div>
             <button className="btn btn-sm btn-outline-secondary">
               <i className="ion-plus-round" />
-              &nbsp; Follow Eric Simons <span className="counter">(10)</span>
+              {/* TODO display follower count */}
+              &nbsp; Follow {article.author.username} <span className="counter">(0)</span>
             </button>
             &nbsp;&nbsp;
             <button className="btn btn-sm btn-outline-primary">
               <i className="ion-heart" />
-              &nbsp; Favorite Post <span className="counter">(29)</span>
+              &nbsp; Favorite Post <span className="counter">({article.favoritesCount})</span>
             </button>
           </div>
         </div>
@@ -31,9 +70,7 @@ export function Article() {
       <div className="container page">
         <div className="row article-content">
           <div className="col-md-12">
-            <p>Web development technologies have evolved at an incredible clip over the past few years.</p>
-            <h2 id="introducing-ionic">Introducing RealWorld.</h2>
-            <p>It&lsquo;s a great solution for learning how other frameworks work.</p>
+            <Markdown>{article.body}</Markdown>
           </div>
         </div>
 
@@ -41,23 +78,23 @@ export function Article() {
 
         <div className="article-actions">
           <div className="article-meta">
-            <a href="/#/profile/ericsimmons">
-              <img src="http://i.imgur.com/Qr71crq.jpg" />
-            </a>
+            <Link to={`/profile/${article.author.username}`}>
+              <img src={article.author.image || userImagePlaceholder} alt="" />
+            </Link>
             <div className="info">
-              <a href="/#/profile/ericsimmons" className="author">
-                Eric Simons
-              </a>
-              <span className="date">January 20th</span>
+              <Link to={`/profile/${article.author.username}`} className="author">
+                {article.author.username}
+              </Link>
+              <span className="date">{format(new Date(article.createdAt), "MMMM do")}</span>
             </div>
             <button className="btn btn-sm btn-outline-secondary">
               <i className="ion-plus-round" />
-              &nbsp; Follow Eric Simons
+              &nbsp; Follow {article.author.username}
             </button>
             &nbsp;
             <button className="btn btn-sm btn-outline-primary">
               <i className="ion-heart" />
-              &nbsp; Favorite Post <span className="counter">(29)</span>
+              &nbsp; Favorite Post <span className="counter">({article.favoritesCount})</span>
             </button>
           </div>
         </div>
